0041c9b32ebafc46c9129487f4c8311504250d64
olivier-tan-ics4u1-02-25 <olivier.tan@student.tdsb.on.ca>
2025-03-16 22:36:56 +0000

    1.2 Completed

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,35 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static boolean validIndex (int[] arr, int index){
 
+	//q1	
+	try	{
+		arr[index] = arr[index];
+		return true;
+	}	
+		catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
 	}
+	//q2
+	public static int divide(int x, int y){
 
-	
+		try{
+			return x/y;
+		}
+		catch (ArithmeticException e){
+			return 0;
+		}
+	} 
+	//q3
+	public static int safeConvertStringtoInt(String word){
+
+		try{
+			return Integer.parseInt(word);
+		}
+		catch (NumberFormatException e){
+			return 0;
+		}
+	}
 
 }
